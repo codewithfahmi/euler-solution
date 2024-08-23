0f221d731c63131defb3228a81479f60032c87ed
codewithfahmi <fahmiyahya0@gmail.com>
2024-08-23 15:39:47 +0700

    add: solution for "multiples of 3 or 5"

---
+++ 01-multiples-of-3-or-5/solution.clj
@@ -0,0 +1,8 @@
+(defn solution []
+  (let [result (->> (range 1000)
+                    (filter (fn [x] (or (== (mod x 3) 0) (== (mod x 5) 0))))
+                    (reduce +))]
+    (println result)))
+
+(println (range 1 1000))
+(solution)
